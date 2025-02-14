ac5e3cf8f3462003bf1ac68ed25c503a615db322
Shikizzz <widemann.simon@outlook.fr>
2025-02-14 10:53:22 +0100

    Added reactivity to Profile form

---
+++ Back/src/main/java/com/orion/mdd/model/dto/auth/ModifyNoPassword.java
@@ -0,0 +1,48 @@
+package com.orion.mdd.model.dto.auth;
+
+
+import com.orion.mdd.config.UsernameValidation;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class ModifyNoPassword {
+    @NotNull
+    private Integer id;
+    @NotBlank(message = "Username may not be null")
+    @UsernameValidation
+    private String username;
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
+            message = "Invalid email")
+    private String email;
+
+    public ModifyNoPassword(Integer id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
